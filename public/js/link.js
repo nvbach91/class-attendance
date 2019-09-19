ee42c548c53bd3335fc90b22f9254a695474e335
Nguyen Viet Bach <nvbach91@outlook.com>
2019-09-20 01:34:46 +0200

    added weeks list

--- public/js/link.js
+++ public/js/link.js
@@ -31,12 +31,31 @@ $(document).ready(function () {
                 <a class="attendance-url" href=${url}>${url}</a>
                 <hr>
                 <div class="d-flex justify-content-between">
-                    <strong>T${validTimes[uuid].column}</strong>
+                    <strong>Week ${validTimes[uuid].column}</strong>
                     <strong>${new Date(validTimes[uuid].date).toLocaleDateString('cs')}</strong>
                 </div>`
             );
         } else {
             qrCodeContainer.before(`<div class="alert alert-danger" role="alert">There is no active attendance session</div>`)
         }
+        
+        var weeks = Object.values(validTimes);
+        weeks.sort((a, b) => a.column - b.column);
+        $('#main').append(`
+            <br>
+            <div class="card">
+                <div class="card-header">Upcoming classes</div>
+                <div class="card-body">
+                ${weeks.map((week) => { 
+                    return `
+                        <button class="upcoming-class btn btn-primary ${now > new Date(week.date) ? 'text-muted' : 'font-weight-bold'}">
+                            <span>Week ${week.column}</span>
+                            <span>${new Date(week.date).toLocaleDateString('cs')}</span>
+                        </button>
+                    `;
+                }).join('')}
+                </div>
+            </div>
+        `)
     });
 });
