a92f7c631e0ea63486b5a708340c962905c8db14
Viet Bach Nguyen <nvbach91@outlook.com>
2023-09-21 21:57:46 +0200

    disable morgan

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(cors());
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
